b5b9a5edb7fdbd3d525e4475287b83b18cdfa563
Ingo Kegel <ingo.kegel@ej-technologies.com>
2018-04-15 22:56:10 +0200

    gradle project 2

---
+++ platform/shim/src/com/intellij/ui/EditorTextField.java
@@ -0,0 +1,11 @@
+package com.intellij.ui;
+
+import com.intellij.ui.components.JBPanel;
+
+import javax.swing.*;
+
+public class EditorTextField extends JBPanel {
+    public JComponent getFocusTarget() {
+        throw new UnsupportedOperationException();
+    }
+}
