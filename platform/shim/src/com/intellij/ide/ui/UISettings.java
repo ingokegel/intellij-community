345fee515eaf159ad0dacef42ae0d1e6b2517169
Ingo Kegel <ingo.kegel@ej-technologies.com>
2018-04-19 18:17:30 +0200

    darcula shim 1

---
+++ platform/shim/src/com/intellij/ide/ui/UISettings.java
@@ -0,0 +1,10 @@
+package com.intellij.ide.ui;
+
+import java.awt.*;
+
+public class UISettings {
+    public static void setupAntialiasing(Graphics g) {
+
+
+    }
+}
---
+++ platform/shim/src/com/intellij/openapi/wm/impl/IdeRootPane.java
@@ -0,0 +1,6 @@
+package com.intellij.openapi.wm.impl;
+
+import javax.swing.*;
+
+public class IdeRootPane extends JRootPane {
+}
