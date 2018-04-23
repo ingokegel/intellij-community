ec1dbacfde8a805d2a9563d9b3e1645d4a995973
Ingo Kegel <ingo.kegel@ej-technologies.com>
2018-04-23 16:26:59 +0200

    first working subset

---
+++ platform/shim/src/com/intellij/ui/components/JBViewport.java
@@ -0,0 +1,10 @@
+package com.intellij.ui.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JBViewport extends JViewport {
+    public static Insets getViewInsets(JComponent view) {
+        return null;
+    }
+}
