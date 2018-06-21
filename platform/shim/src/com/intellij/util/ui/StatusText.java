a828896ef7f644d0ee4b928778e8c6998aa22811
Ingo Kegel <ingo.kegel@ej-technologies.com>
2018-06-21 16:57:06 +0200

    Include JBViewPort

---
+++ platform/shim/src/com/intellij/util/ui/StatusText.java
@@ -0,0 +1,19 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.util.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StatusText {
+  public void attachTo(Component owner, Component mouseTarget) {
+
+  }
+
+  public void attachTo(Component owner) {
+
+  }
+
+  public void paint(Component viewport, Graphics g) {
+
+  }
+}
