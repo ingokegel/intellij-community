4669782e5b2864f3a412edf0a789b2e4ccd9c4bf
Ingo Kegel <ingo.kegel@ej-technologies.com>
2018-07-08 12:35:01 +0200

    Fixes after rebase on upstream/182

--- platform/util/util.gradle.kts
+++ platform/util/util.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     compile("org.apache.commons:commons-compress:1.16.1")
     compile("net.jpountz.lz4:lz4:1.3.0")
     compile("net.java.dev.jna:jna-platform:4.5.0")
-    compile("org.apache.xmlgraphics:batik-all:1.9.1")
+    compile("org.apache.xmlgraphics:batik-all:1.10")
     compile("oro:oro:2.0.8")
     compile("org.imgscalr:imgscalr-lib:4.2")
     compile("org.jetbrains.intellij.deps:jdom:2.0.6")
