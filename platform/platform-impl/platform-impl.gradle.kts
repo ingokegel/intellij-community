d7184b2e98d63a0502e63ee18ee842335c590750
Ingo Kegel <ingo.kegel@ej-technologies.com>
2018-09-20 11:07:31 +0200

    Gradle 4.10.1

--- platform/platform-impl/platform-impl.gradle.kts
+++ platform/platform-impl/platform-impl.gradle.kts
@@ -11,36 +11,30 @@ dependencies {
     compile(project(":platform-api"))
 }
 
-java {
-    sourceSets {
-        "main" {
-            java {
-                include("com/intellij/ide/ui/laf/**")
-                exclude(
-                        "com/intellij/ide/ui/laf/darcula/ui/DarculaOptionButtonUI.kt",
-                        "com/intellij/ide/ui/laf/intellij/MacIntelliJOptionButtonUI.kt",
-                        "com/intellij/ide/ui/laf/intellij/WinIntelliJOptionButtonUI.kt",
-                        "com/intellij/ide/ui/laf/intellij/WinOnOffButtonUI.java"
-                )
+sourceSets["main"].java {
+    include("com/intellij/ide/ui/laf/**")
+    exclude(
+            "com/intellij/ide/ui/laf/darcula/ui/DarculaOptionButtonUI.kt",
+            "com/intellij/ide/ui/laf/intellij/MacIntelliJOptionButtonUI.kt",
+            "com/intellij/ide/ui/laf/intellij/WinIntelliJOptionButtonUI.kt",
+            "com/intellij/ide/ui/laf/intellij/WinOnOffButtonUI.java"
+    )
 
-                include("com/intellij/ui/plaf/**")
-                include(
-                        "com/intellij/ui/WindowMoveListener.java",
-                        "com/intellij/ui/WindowMouseListener.java",
-                        "com/intellij/ui/WindowResizeListener.java",
-                        "com/intellij/ui/ColoredSideBorder.java",
-                        "com/intellij/ui/mac/MacPopupMenuUI.java"
-                )
-            }
-        }
-    }
+    include("com/intellij/ui/plaf/**")
+    include(
+            "com/intellij/ui/WindowMoveListener.java",
+            "com/intellij/ui/WindowMouseListener.java",
+            "com/intellij/ui/WindowResizeListener.java",
+            "com/intellij/ui/ColoredSideBorder.java",
+            "com/intellij/ui/mac/MacPopupMenuUI.java"
+    )
 }
 
 tasks {
 
     val jar by getting(Jar::class)
 
-    "dist"(Copy::class) {
+    create<Copy>("dist") {
         dependsOn(jar)
         from(configurations.compile.files.filterNot { file ->
             listOf(
@@ -61,7 +55,7 @@ tasks {
     }
 
 
-    "processResources"(Copy::class) {
+    named<Copy>("processResources") {
         from("src") {
             include("**/com/intellij/ide/ui/laf/**/*.properties")
             include("**/com/intellij/ide/ui/laf/**/*.css")
