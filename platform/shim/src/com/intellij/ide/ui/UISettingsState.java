7fc410f3ffc70bbb18e705b02079473747e47aa2
Ingo Kegel <ingo.kegel@ej-technologies.com>
2019-02-27 18:18:05 +0100

    Fix after rebase on 2018.3.5

---
+++ platform/shim/src/com/intellij/ide/ui/UISettingsState.java
@@ -0,0 +1,15 @@
+package com.intellij.ide.ui;
+
+public class UISettingsState {
+    public boolean getOverrideLafFonts() {
+        return false;
+    }
+
+    public String getFontFace() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getFontSize() {
+        throw new UnsupportedOperationException();
+    }
+}
